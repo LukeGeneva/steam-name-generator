ade4d5fbe16949d1a4d753c86da87ba6aedf45b1
Luke Geneva <lukegeneva@gmail.com>
2017-03-22 14:40:36 -0500

    Limit to contemporary English dictionary

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 const request = require('request');
 
-const ADJ_LIMIT = 51688;
-const NOUN_LIMIT = 172504;
+const ADJ_LIMIT = 8771;
+const NOUN_LIMIT = 27900;
 
 const adjOffset = randomInt(ADJ_LIMIT);
 const nounOffset = randomInt(NOUN_LIMIT);
-const adjUrl = 'http://api.pearson.com/v2/dictionaries/entries?part_of_speech=adjective&limit=1&offset=' + adjOffset.toString();
-const nounUrl = 'http://api.pearson.com/v2/dictionaries/entries?part_of_speech=noun&limit=1&offset=' + nounOffset.toString();
+const adjUrl = 'http://api.pearson.com/v2/dictionaries/ldoce5/entries?part_of_speech=adjective&limit=1&offset=' + adjOffset.toString();
+const nounUrl = 'http://api.pearson.com/v2/dictionaries/ldoce5/entries?part_of_speech=noun&limit=1&offset=' + nounOffset.toString();
 
 request(adjUrl, function(error, response, body) {
   let result = JSON.parse(body);
